3c85b5f956e9d25caa991ca0cf3b3ef753e1f333
johnsiraani <hi@johnsiraani.com>
2014-11-01 11:08:06 +0530

    constructed insert query

--- insert.php
+++ insert.php
@@ -1,2 +1,9 @@
 <?php
 include "connect.php";
+$name = "john O' conner";
+$email = 'john@gmail.com';
+$dob = '1992/11/23';
+$phoneno = '8849406779';
+$password = 'johh';
+$insertQuery = "INSERT INTO persons (name, email, dateofbirth, phonenumber, image, password) VALUES ('$name', '$email', '$dob', '$phoneno', '$image', '$password')";    
+echo $insertQuery;
