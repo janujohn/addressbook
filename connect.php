b5d1cb4c096dd9cd3d129c028cfc0525ba974ab2
johnsiraani <hi@johnsiraani.com>
2014-11-01 11:05:16 +0530

    mysql_connect and mysql_select_db are stored in variables

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect("localhost" , "root" , "root") or die(mysql_error());
-mysql_select_db("persondetails") or die("cannot connect to database");
+$link = mysql_connect("localhost" , "root" , "root") or die(mysql_error());
+$db = mysql_select_db("persondetails") or die("cannot connect to database");
 ?>
