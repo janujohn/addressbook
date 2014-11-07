bd29e193d4a02aa8603265f8cfbfb118fe74a6a8
johnsiraani <hi@johnsiraani.com>
2014-11-07 08:36:40 +0530

    * dummy data removed and added post variables * user input data sanitized * restricted image extension * insert database logic

--- insert.php
+++ insert.php
@@ -1,9 +1,34 @@
 <?php
 include "connect.php";
-$name = "john O' conner";
-$email = 'john@gmail.com';
-$dob = '1992/11/23';
-$phoneno = '8849406779';
-$password = 'johh';
-$insertQuery = "INSERT INTO persons (name, email, dateofbirth, phonenumber, image, password) VALUES ('$name', '$email', '$dob', '$phoneno', '$image', '$password')";    
-echo $insertQuery;
+$name = mysql_escape_string($_POST['name']);
+$email = mysql_escape_string($_POST['email']);
+$dob = mysql_escape_string($_POST['dateofbirth']);
+$phoneno = mysql_escape_string($_POST['phoneno']);
+$image = $_FILES['image']['name'];
+$imageuniqname = sha1(uniqid(). $image . time());
+$imagearray = explode ('.', $image);
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$ext = $imagearray[count($imagearray)-1];
+if (in_array($ext, $allowedExts))
+{
+	$destimagename = $imageuniqname . '.' . $ext;
+
+	$target_folder = 'images/';
+
+	if(is_uploaded_file($_FILES['image']['tmp_name'])) {
+		move_uploaded_file($_FILES['image']['tmp_name'], $target_folder . $destimagename) or die ('cannot upload');
+	}
+
+}
+else
+{
+	echo "Invalid file extension";
+}
+
+$address = mysql_escape_string($_POST['address']);
+
+$insertQuery = "INSERT INTO persons (name, email, dateofbirth, phonenumber, image, address) VALUES ('$name', '$email', '$dob', '$phoneno', '$destimagename', '$address')";    
+
+$insertOk = mysql_query($insertQuery);
+
+?>
