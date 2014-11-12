c1c5be626cf76f322df0235b5f8349d83e49c684
johnsiraani <hi@johnsiraani.com>
2014-11-12 09:47:59 +0530

    added header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+       
+        <title>Registration Page</title>
+        <!-- BOOTSTRAP CORE STYLE CSS -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <!-- FONTAWESOME STYLE CSS -->
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+        <!-- CUSTOM STYLE CSS -->
+        <link href="assets/css/style.css" rel="stylesheet" />    
+        <!-- GOOGLE FONT -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    </head>
+
+    <body>
+        <div class="container">
