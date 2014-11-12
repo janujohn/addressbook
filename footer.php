04ee814789c9616e9e01d3ca882fdfadd26691a1
johnsiraani <hi@johnsiraani.com>
2014-11-12 09:39:53 +0530

    added footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div>
+
+     
+
+      
+<!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+        <!-- CORE JQUERY  -->
+        <script src="assets/plugins/jquery-1.10.2.js"></script>
+        <!-- BOOTSTRAP SCRIPTS  -->
+        <script src="assets/plugins/bootstrap.js"></script>
+   
+    </body>
+</html>
